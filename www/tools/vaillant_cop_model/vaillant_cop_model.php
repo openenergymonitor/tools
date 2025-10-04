87c32cddeeb80e8c226ebb146e99c393d3d5fc67
Trystan Lea <trystan.lea@gmail.com>
2025-10-04 15:22:22 +0100

    simulated cop comparison

--- www/tools/vaillant_cop_model/vaillant_cop_model.php
+++ www/tools/vaillant_cop_model/vaillant_cop_model.php
@@ -3,7 +3,7 @@
 
 <?php $title = "Vaillant COP model"; ?>
 
-<div class="container mt-3" style="max-width:800px" id="app">
+<div class="container mt-3" style="max-width:1000px" id="app">
     <div class="row">
         <div class="col">
             <h3>Vaillant COP model</h3>
@@ -35,8 +35,8 @@
                         <td><b>{{ ambient_temp }}</b></td>
                         <td v-for="(speed, speed_index) in data['5kW'].speed" :style="{backgroundColor: getCopColor(data['5kW']['35C'].cop[amb_index][speed_index])}">
                             <div v-if="data['5kW']['35C'].cop[amb_index][speed_index] !== null">
-                                <b>{{ data['5kW']['35C'].cop[amb_index][speed_index] }}</b>
-                                <br>
+                                <b>{{ data['5kW']['35C'].cop[amb_index][speed_index] }}</b> 
+                                <small>({{ data['5kW']['35C'].sim_cop[amb_index][speed_index] }})</small><br>
                                 <small>{{ data['5kW']['35C'].output[amb_index][speed_index] }} kW</small>
                             </div>
                         </td>
@@ -59,7 +59,51 @@
                 this.model();
             },
             model: function() {
+                // Generate modelled COP using carnot COP equation
+                for (var model in this.data) {
 
+                    var total_error = 0;
+                    var count = 0;
+                    
+                    var model_data = this.data[model];
+
+                    for (var flow_temp_str in model_data) {
+                        if (flow_temp_str.endsWith('C')) {
+                            var flow_temp_data = model_data[flow_temp_str];
+                            var T_flow = parseFloat(flow_temp_str);
+
+                            if (!flow_temp_data.sim_cop) {
+                                this.$set(flow_temp_data, 'sim_cop', []);
+                            }
+
+                            for (var i = 0; i < model_data.ambient.length; i++) {
+                                if (!flow_temp_data.sim_cop[i]) {
+                                    this.$set(flow_temp_data.sim_cop, i, []);
+                                }
+                                var T_ambient = model_data.ambient[i];
+
+                                for (var j = 0; j < model_data.speed.length; j++) {
+                                    if (flow_temp_data.cop[i][j] !== null) {
+
+                                        let speed = model_data.speed[j];
+                                        let condensing_offset = 4 * (speed / 120);
+                                        let evaporating_offset = -10 * (speed / 120);
+
+                                        var T_condensing = T_flow + condensing_offset;
+                                        var T_evaporating = T_ambient + evaporating_offset;
+
+                                        var carnot_cop = (T_condensing + 273.15) / (T_condensing - T_evaporating);
+                                        var practical_cop = carnot_cop * 0.52;
+
+                                        this.$set(flow_temp_data.sim_cop[i], j, practical_cop.toFixed(1));
+                                    } else {
+                                        this.$set(flow_temp_data.sim_cop[i], j, null);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             },
             getCopColor: function(cop) {
                 if (cop === null) return '#f8f9fa';
