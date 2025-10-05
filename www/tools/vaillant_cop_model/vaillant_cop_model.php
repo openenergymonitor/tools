9589e602cd4949739e5e09597ae91e0de788e9f4
Trystan Lea <trystan.lea@gmail.com>
2025-10-05 17:35:21 +0100

    vapour compression cycle option

--- www/tools/vaillant_cop_model/vaillant_cop_model.php
+++ www/tools/vaillant_cop_model/vaillant_cop_model.php
@@ -1,6 +1,7 @@
 <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
 <script src="<?php echo $path_lib;?>vaillant5.js?v=10"></script>
 
+
 <?php $title = "Vaillant COP model"; ?>
 
 <div class="container mt-3" style="max-width:1000px" id="app">
@@ -21,18 +22,18 @@
             <table class="table table-bordered table-sm text-center">
                 <thead>
                     <tr>
-                        <th>Ambient °C</th>
-                        <th v-for="speed in data['5kW'].speed">{{ speed }} rps</th>
+                        <th class="fs-6">Ambient °C</th>
+                        <th class="fs-6" v-for="speed in data['5kW'].speed">{{ speed }} rps</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="(ambient_temp, amb_index) in data['5kW'].ambient">
-                        <td><b>{{ ambient_temp }}</b></td>
+                        <td><b class="fs-6">{{ ambient_temp }}</b></td>
                         <td v-for="(speed, speed_index) in data['5kW'].speed" :style="{backgroundColor: getCopColor(data['5kW'][active_flow_temp].cop[amb_index][speed_index])}">
                             <div v-if="data['5kW'][active_flow_temp].cop[amb_index][speed_index] !== null">
-                                <b>{{ data['5kW'][active_flow_temp].cop[amb_index][speed_index] }}</b> 
-                                <small v-if="data['5kW'][active_flow_temp].sim_cop">({{ data['5kW'][active_flow_temp].sim_cop[amb_index][speed_index] }})</small><br>
-                                <small>{{ data['5kW'][active_flow_temp].output[amb_index][speed_index] }} kW</small>
+                                <b class="fs-6">{{ data['5kW'][active_flow_temp].cop[amb_index][speed_index] }}</b> 
+                                <small class="fs-7" v-if="data['5kW'][active_flow_temp].sim_cop">({{ data['5kW'][active_flow_temp].sim_cop[amb_index][speed_index] }})</small>
+                                <small class="d-block fs-7">{{ data['5kW'][active_flow_temp].output[amb_index][speed_index] }} kW</small>
                             </div>
                         </td>
                     </tr>
@@ -88,17 +89,36 @@
                 <option value="carnot-fixed-offset">Carnot with fixed offsets</option>
                 <option value="carnot-variable-offset">Carnot with variable offsets (scaled by speed)</option>
                 <option value="carnot-variable-offset-output">Carnot with variable offsets (scaled by output)</option>
+                <option value="coolprop-vapour-compression">CoolProp vapour compression model</option>
                 <option value="vaillant-datasheet">Vaillant datasheet interpolation (validation only)</option>
             </select>
         </div>
 
-        <div class="col" v-if="cop_model === 'carnot-variable-offset' || cop_model === 'carnot-fixed-offset'">
+        <div class="col" v-if="cop_model === 'carnot-variable-offset' || cop_model === 'carnot-fixed-offset' || cop_model === 'carnot-variable-offset-output'">
             <label class="form-label">Practical COP factor</label>
             <div class="input-group mb-3">
                 <input type="text" class="form-control" v-model.number="practical_cop_factor" @change="update()">
                 <span class="input-group-text">× Carnot COP</span>
             </div>
         </div>
+
+        <div class="col" v-if="cop_model === 'coolprop-vapour-compression'">
+            <label class="form-label">Refrigerant</label>
+            <select class="form-select" v-model="refrigerant" @change="update()">
+                <option value="R290">R290</option>
+                <option value="R32">R32</option>
+                <option value="R410A">R410A</option>
+            </select>
+        </div>
+
+        <div class="col" v-if="cop_model === 'coolprop-vapour-compression'">
+            <label class="form-label">Compressor isentropic efficiency</label>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" v-model.number="eta_isentropic" @change="update()">
+                <span class="input-group-text">η</span>
+            </div>
+        </div>
+
 
     </div>
     <div class="row mt-3">
@@ -119,7 +139,7 @@
             </div>
         </div>
 
-        <div class="col" v-if="cop_model === 'carnot-variable-offset-output'">
+        <div class="col" v-if="cop_model === 'carnot-variable-offset-output' || cop_model === 'coolprop-vapour-compression'">
             <label class="form-label">Condensing temp scale (°C at max output)</label>
             <div class="input-group mb-3">
                 <input type="text" class="form-control" v-model.number="condensing_scale" @change="update()">
@@ -127,7 +147,7 @@
             </div>
         </div>
 
-        <div class="col" v-if="cop_model === 'carnot-variable-offset-output'">
+        <div class="col" v-if="cop_model === 'carnot-variable-offset-output' || cop_model === 'coolprop-vapour-compression'">
             <label class="form-label">Evaporating temp scale (°C at max output)</label>
             <div class="input-group mb-3">
                 <input type="text" class="form-control" v-model.number="evaporating_scale" @change="update()">
@@ -135,7 +155,7 @@
             </div>
         </div>
 
-        <div class="col" v-if="cop_model === 'carnot-variable-offset-output'">
+        <div class="col" v-if="cop_model === 'carnot-variable-offset-output' || cop_model === 'coolprop-vapour-compression'">
             <label class="form-label">Max heat output for scaling</label>
             <div class="input-group mb-3">
                 <input type="text" class="form-control" v-model.number="max_output" @change="update()">
@@ -158,8 +178,6 @@
                 <span class="input-group-text">°C</span>
             </div>
         </div>
-
-
     </div>
 </div>
 
@@ -182,7 +200,9 @@
             // variable offsets scaled by output
             max_output: 8.5, // kW for 5kW model
             // multiplier to get practical COP from Carnot COP
-            practical_cop_factor: 0.45
+            practical_cop_factor: 0.45,
+            eta_isentropic: 0.51, // Compressor isentropic efficiency
+            refrigerant: 'R290', // CoolProp refrigerant
         },
         computed: {
             flow_temps: function() {
@@ -223,6 +243,63 @@
                 let carnot_cop = (T_condensing + 273.15) / (T_condensing - T_evaporating);
                 return practical_cop = carnot_cop * this.practical_cop_factor;
             },
+            coolprop_vapour_compression_cop: function(T_flow, T_ambient, output) {
+
+                let condensing_offset = (output / this.max_output) * this.condensing_scale;
+                let evaporating_offset = (output / this.max_output) * this.evaporating_scale;
+
+                // T_flow: Desired outlet temperature (e.g., water temp) in °C
+                // T_ambient: Heat source temperature (e.g., ambient air temp) in °C
+                
+                // Assumptions for saturation temperatures (°C)
+                let T_condensing = T_flow + condensing_offset; 
+                let T_evaporating = T_ambient + evaporating_offset;
+
+                // Convert all temperatures to Kelvin for CoolProp
+                const T_cond_K = T_condensing + 273.15;
+                const T_evap_K = T_evaporating + 273.15;
+
+                const CoolProp = Module;
+                const fluid = this.refrigerant || 'R290'; // Propane
+
+                // --- 1. Determine Pressures ---
+                // P_condensing: Saturated liquid (Q=0) pressure at T_condensing
+                let p_condensing = CoolProp.PropsSI('P', 'T', T_cond_K, 'Q', 0, fluid); 
+                // P_evaporating: Saturated vapor (Q=1) pressure at T_evaporating
+                let p_evaporating = CoolProp.PropsSI('P', 'T', T_evap_K, 'Q', 1, fluid); 
+
+                // --- 2. Cycle Point Enthalpies (J/kg) ---
+                // h1 (State 1: Compressor Inlet)
+                // Saturated vapor at evaporating pressure
+                let h1 = CoolProp.PropsSI('H', 'P', p_evaporating, 'Q', 1, fluid); 
+                let s1 = CoolProp.PropsSI('S', 'P', p_evaporating, 'Q', 1, fluid); // Needed for isentropic process
+
+                // h3 (State 3: Condenser Outlet)
+                // Saturated liquid at condensing pressure (subcooling assumed zero)
+                let h3 = CoolProp.PropsSI('H', 'P', p_condensing, 'Q', 0, fluid); 
+
+                // h4 (State 4: Evaporator Inlet)
+                // Isenthalpic expansion: h4 = h3
+                let h4 = h3; 
+
+                // --- 3. Isentropic and Actual Compression (h2) ---
+                // h2s (Isentropic State 2)
+                // Isentropic process: s2s = s1 at P_condensing
+                let h2s = CoolProp.PropsSI('H', 'P', p_condensing, 'S', s1, fluid); 
+
+                // h2 (Actual State 2)
+                // Actual Work = Isentropic Work / Efficiency
+                // h2 = h1 + (h2s - h1) / eta_isentropic
+                let h2 = h1 + (h2s - h1) / this.eta_isentropic; 
+
+                // --- 4. COP Calculation ---
+                // COP (Heating) = Q_condenser / W_compressor
+                // COP = (h2 - h3) / (h2 - h1)
+                let cop = (h2 - h3) / (h2 - h1);
+
+                return cop;
+            },
+
             model: function() {
                 // Generate modelled COP using carnot COP equation
                 for (var model in this.data) {
@@ -265,6 +342,9 @@
                                         } else if (this.cop_model === 'vaillant-datasheet') {
                                             let output = flow_temp_data.output[i][j];
                                             practical_cop = getCOP(vaillant_data, T_flow, T_ambient, output);
+                                        } else if (this.cop_model === 'coolprop-vapour-compression') {
+                                            let output = flow_temp_data.output[i][j];
+                                            practical_cop = this.coolprop_vapour_compression_cop(T_flow, T_ambient, output);
                                         }
 
                                         if (practical_cop !== null) {
@@ -302,3 +382,4 @@
     });
     app.model();
 </script>
+<script src="<?php echo $path;?>coolprop/coolprop.js"></script>
